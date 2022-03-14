928fb0def765f64a1688b9de9c8a1458ddcf20af
Chioma-d <chiomaokoene@gmail.com>
2022-03-14 10:18:24 -0700

    tech work

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints exactly "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
