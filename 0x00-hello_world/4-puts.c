af6d53ae4423022bbfc7311da44619edf7c08bd0
Chioma-d <chiomaokoene@gmail.com>
2022-03-14 09:32:15 -0700

    tech work

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out strings to stdout
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}		
