5bb09b32cd1360e8a4d33e4b79f6655910c56771
Chioma-d <chiomaokoene@gmail.com>
2022-03-15 04:27:02 -0700

    working with c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ * main - Prints the alphabetic
+ * but omits e and q
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
